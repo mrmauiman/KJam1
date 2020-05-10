6ee5f8642b9a9d94a1fc696881b113185c6b5238
Maui <mlkelley@mail.roanoke.edu>
2020-05-10 15:25:38 -0400

    Money interactions

--- Project/Scenes/KinematicBody.gd
+++ Project/Scenes/KinematicBody.gd
@@ -2,60 +2,64 @@ extends KinematicBody
 
 var controller = "controller_0" setget set_controller, get_controller
 var is_key_board = false setget set_is_key_board, get_is_key_board
-var player = 0
 
 const gravity = 10
 const max_fall_speed = 20
 const max_move_speed = 15
 const acceleration = 15
 const turn_speed = 5 # degrees
-const friction = 0.5
+const friction = 0.2
 const up = Vector3.UP
 
-enum states {FREE_MOVE, SUCKING, HOLDING}
+#enum states {FREE_MOVE, SUCKING, HOLDING}
 
 var speed = 0
+var rot = Vector2(1, 0)
 var velocity = Vector3.ZERO
-var state = states.FREE_MOVE
+#var state = states.FREE_MOVE
 
 # helpers
 func move_input():
 	# input
-	var move_vec = Vector3.ZERO
+	var input_vec = Vector2.ZERO
 	if is_key_board:
-		move_vec = Vector3(Input.is_action_pressed("key_board_up") as int - Input.is_action_pressed("key_board_down") as int, 0, Input.is_action_pressed("key_board_right") as int - Input.is_action_pressed("key_board_left") as int)
+		input_vec = Vector2(Input.is_action_pressed("key_board_up") as int - Input.is_action_pressed("key_board_down") as int, Input.is_action_pressed("key_board_right") as int - Input.is_action_pressed("key_board_left") as int)
 	else :
-		move_vec = Vector3(Input.get_action_strength(controller + "_up")-Input.get_action_strength(controller + "_down"), 0, Input.get_action_strength(controller + "_right")-Input.get_action_strength(controller + "_left"))
-	if move_vec.length() >= 1:
-		move_vec = move_vec.normalized()
-	return move_vec
+		input_vec = Vector2(Input.get_action_strength(controller + "_up")-Input.get_action_strength(controller + "_down"), Input.get_action_strength(controller + "_right")-Input.get_action_strength(controller + "_left"))
+	if input_vec.length() >= 1:
+		input_vec = input_vec.normalized()
+	return input_vec
 
 # State Functions
-func free_move():
+func free_move(delta):
 	# gravity
 	velocity.y = clamp(velocity.y - gravity, -max_fall_speed, 0)
 	var input_vec = move_input()
 	
-	speed = speed + (max_move_speed * input_vec.length())
-	velocity.z = speed
-	# rotate(vec3, angle)
+	if input_vec.length() > 0:
+		var angle = rot.normalized().angle_to(input_vec.normalized())
+		if angle >= deg2rad(3):
+			rot = rot.rotated(turn_speed*delta)
+		elif angle <= deg2rad(-3):
+			rot = rot.rotated(-turn_speed*delta)
+		rotation = Vector3(0, (deg2rad(360) - rot.angle()) + deg2rad(90), 0)
 	
-#	# create acceleration vector
-#	move_vec = move_vec * acceleration
-#	velocity = velocity + move_vec
-#	# friction
-#	if move_vec.length() == 0:
-#		velocity = lerp(velocity, Vector3(0, velocity.y, 0), friction)
-#		if Vector2(velocity.x, velocity.z).length() < 0.1:
-#			velocity.x = 0
-#			velocity.z = 0
-#	# clamp to max speed
-#	var clamp_vel = Vector2(velocity.x, velocity.z).clamped(max_move_speed);
-#	velocity.x = clamp_vel.x
-#	velocity.z = clamp_vel.y
+	speed = clamp(speed + (input_vec.length()*acceleration), -max_move_speed, max_move_speed)
+	if input_vec.length() == 0:
+		speed = lerp(speed, 0, friction)
+		if (speed < 0.1):
+			speed = 0
+	print(speed)
+	
+	rot = rot.normalized()
+	velocity.x = rot.x * speed
+	velocity.z = rot.y * speed
 
-func sucking():
-	pass
+func sucking(delta):
+	if Input.is_action_pressed(controller + "_suck"):
+		$Suck.visible = true
+	else:
+		$Suck.visible = false
 
 func holding():
 	pass
@@ -66,15 +70,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	match (state):
-		states.FREE_MOVE:
-			free_move()
-		states.SUCKING:
-			sucking()
-		states.HOLDING:
-			holding()
+#	match (state):
+#		states.FREE_MOVE:
+#			free_move(delta)
+#		states.SUCKING:
+#			sucking()
+#		states.HOLDING:
+#			holding()
+	free_move(delta)
+	sucking(delta)
 	move_and_slide(velocity, up)
-	print(velocity)
 
 # Setters and Getters
 func set_controller(new_controller):
